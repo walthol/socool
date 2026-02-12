3b68182ecfc6962a33601664d296700ec1803668
walthol <96192081+walthol@users.noreply.github.com>
2026-02-12 11:18:30 -0500

    Change proxy target from Discord to Google

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://discord.com/"; 
+  let target = "https://google.com/"; 
 
   createProxyMiddleware({
     target,
