1240318548ca1536567c7141cd8983e152e40f3d
walthol <96192081+walthol@users.noreply.github.com>
2026-02-12 11:30:59 -0500

    Change proxy target from Google to Discord

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
   // Replace the URL below with the site you want to unblock
-  let target = "https://www.google.com/"; 
+  let target = "https://www.discord.com/"; 
 
   // This part handles the "handshake" between your Chromebook and the site
   createProxyMiddleware({
